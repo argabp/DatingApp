115ece306266f22da9f7c875e2ae922142f8650f
Arga Bagus Priambodo <arga.priambodo@sinergymediaintegrasi.com>
2019-07-09 18:03:48 +0700

    End of Section 14

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,15 +1,28 @@
 using System;
+using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace DatingApp.API.Headers
 {
     public static class Extensions
     {
-        public static void AddApplicationError(this HttpResponse respone, string message)
+        public static void AddApplicationError(this HttpResponse response, string message)
         {
-            respone.Headers.Add("Application-Error", message);
-            respone.Headers.Add("Access-Control-Expose-Header", "Application-Error");
-            respone.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static void AddPagination(this HttpResponse response, 
+            int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            var camelCaseFormator = new JsonSerializerSettings();
+            camelCaseFormator.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader, camelCaseFormator));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
 
         public static int CalculateAge(this DateTime theDateTime)
